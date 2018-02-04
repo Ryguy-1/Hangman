1935a3c4718f71f332155bcba6fc15e93ea5e820
Ryguy-1 <league@WorthyMini-5.attlocal.net>
2018-02-04 12:59:56 -0800

    sdfa

--- src/Main.java
+++ src/Main.java
@@ -16,18 +16,19 @@ import javax.swing.JPanel;
 public class Main implements KeyListener{
 	
 	static int lives = 10;
-	String word = "";
+	static String word = "";
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel labelLives = new JLabel();
 	JLabel labelWord = new JLabel();
 	static ArrayList<String> list = new ArrayList<String>();
-	static Stack<String> stack = new Stack<String>();
-	static ArrayList<Character> letters;
 	Font font = new Font(labelLives.getFont().getName(),labelLives.getFont().getStyle(), 50);
 	Font font2 = new Font(labelWord.getFont().getName(),labelWord.getFont().getStyle(), 100);
 	static String oldNum = "";
 	static int rounds = 0;
+	static String realWord = "";
+	static String tempWord = "";
+	static int counter = 0;
 public static void main(String[] args) {
 	Main main = new Main();
 	main.setup();
@@ -37,7 +38,7 @@ public static void main(String[] args) {
 		try {
 			Random rand = new Random();
 			int randy = rand.nextInt(3000);
-			BufferedReader br = new BufferedReader(new FileReader("/Hangman/src/dictionary.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("/Users/league/Desktop/Hangman/src/dictionary.txt"));
 			String line = br.readLine();
 			int ctr = 0;
 			
@@ -48,6 +49,7 @@ public static void main(String[] args) {
 					
 				}else {
 					list.add(line);
+					
 					break;
 				}
 			}
@@ -58,18 +60,20 @@ public static void main(String[] args) {
 		}
 			
 	}
-	
-	for (int i = 0; i < list.size(); i++) {
-		stack.push(list.get(i));
+	realWord = list.get(0);
+	System.out.println(realWord);
+	for (int i = 0; i < realWord.length(); i++) {
+			word+="-";
+			main.updateText();
 	}
 	
 	
-
-
 }
 
+
+
+
 void setup() {
-	word = "HELLO";
 	frame.add(panel);
 	frame.addKeyListener(this);
 	frame.setVisible(true);
@@ -83,6 +87,12 @@ void setup() {
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 }
+void updateText() {
+	labelWord.setText(System.lineSeparator()+word);
+	labelLives.setText("Lives: "+lives);
+	frame.pack();
+	
+}
 
 @Override
 public void keyTyped(KeyEvent e) {
@@ -93,19 +103,37 @@ public void keyTyped(KeyEvent e) {
 @Override
 public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
-	letters = new ArrayList<Character>();
-	String tempWord = stack.pop();
-	for(int i = 0;i<tempWord.length();i++) {
-		if(e.getKeyChar()==tempWord.charAt(i)) {
-			letters.add(tempWord.charAt(i));
-			
+	System.out.println(word);
+	if(realWord.contains(""+e.getKeyChar())) {
+		for (int i = 0; i < realWord.length(); i++) {
+			if(realWord.charAt(i)==e.getKeyChar()) {
+				tempWord+=e.getKeyChar();
+			}else {
+				tempWord+=word.charAt(i);
+			}
 		}
+		word=tempWord;
+		tempWord = "";
+	}else {
+		lives-=1;
 	}
-	for (int i = 0; i < tempWord.length(); i++) {
-		word+="_";
+	System.out.println(word);
+	System.out.println(realWord);
+	if(word==realWord) {
+		
+		counter+=1;
+		realWord=list.get(counter);
+		for (int i = 0; i < realWord.length(); i++) {
+			word+="-";
+		}
+		tempWord = "";
+		
+		//need to work out how to go to the next word
 	}
+	updateText();
+	
+	
 }
-
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
